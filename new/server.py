e15cf3f2a3b4425b5e20c6303eb1e66983cc4962
Mahesh S <124417739+Maheshzero@users.noreply.github.com>
2025-04-25 23:06:38 +0530

    Create server.py

---
+++ new/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import yt_dlp
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('test.html' )
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route("/submit", methods=["POST"])
+def submit():     
+  return f"{request.form.get("link")}"  
